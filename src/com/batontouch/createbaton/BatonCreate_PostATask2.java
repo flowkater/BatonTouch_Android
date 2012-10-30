e639e2e8fa27bc3af23738342ebb5f125739da04
Minsu Daniel Kim <minsu.daniel.kim@gmail.com>
2012-10-30 23:30:31 +0900

    UI작업!

--- src/com/batontouch/createbaton/BatonCreate_PostATask2.java
+++ src/com/batontouch/createbaton/BatonCreate_PostATask2.java
@@ -1,20 +1,28 @@
 package com.batontouch.createbaton;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.batontouch.R;
 
 public class BatonCreate_PostATask2 extends Activity {
+	
+	EditText expectedtime;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.batoncreate_postatask2);
-		
+		expectedtime = (EditText) findViewById(R.id.expectedtime);
 
 	}
 	
@@ -23,6 +31,24 @@ public class BatonCreate_PostATask2 extends Activity {
 		Intent intent = new Intent(getApplicationContext(), BatonCreate_PostATask3.class);
 		startActivity(intent);
 		
+	}
+	
+	public void expectedTime(View v){
+		
+		Toast.makeText(getApplicationContext(), "Clicked", 3000).show();
+		new AlertDialog.Builder(this).setItems(R.array.expecttime,
+
+		new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				String[] distance = getResources().getStringArray(
+						R.array.expecttime);
+
+				expectedtime.setText(distance[which] + "");
+			}
+		}).setNegativeButton("취소", null).show();
+		
+		InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(expectedtime.getWindowToken(), 0);
 	}
 
 }
--- src/com/batontouch/createbaton/BatonCreate_PostATask3.java
+++ src/com/batontouch/createbaton/BatonCreate_PostATask3.java
@@ -1,33 +1,117 @@
 package com.batontouch.createbaton;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.batontouch.R;
-import com.batontouch.homeindex.BatonIndexActivity;
-import com.batontouch.main.MainActivity;
 
 public class BatonCreate_PostATask3 extends Activity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+
+		loadApps();
+
 		setContentView(R.layout.batoncreate_postatask3);
+		GridView grid = (GridView) findViewById(R.id.gridView);
+		
+		// LayoutInflater li = getLayoutInflater();
+				
+		
+		
+
+		grid.setAdapter(new AppsAdapter());
+		
+		
+	}
+
+
+
+	ArrayList<Integer> mApps = new ArrayList<Integer>();
+
+	private void loadApps() {
+		Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+		mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+		// mApps = getPackageManager().queryIntentActivities(mainIntent, 0);
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.tomntoms);
+		mApps.add(R.drawable.tomntoms);
+		mApps.add(R.drawable.tomntoms);
+		
+		mApps.add(R.drawable.coffeebean);
+		mApps.add(R.drawable.coffeebean);
+		mApps.add(R.drawable.coffeebean);
+		
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.starbucks);
+		mApps.add(R.drawable.tomntoms);
+		mApps.add(R.drawable.tomntoms);
+		mApps.add(R.drawable.tomntoms);
+		
+		mApps.add(R.drawable.coffeebean);
+		mApps.add(R.drawable.coffeebean);
+		mApps.add(R.drawable.coffeebean);
 		
 
 	}
-	
-	public void mOnClick(View v){
-		
-		Toast.makeText(getApplicationContext(), "일 등록 완료!", 3000).show();
-		
-		Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-		startActivity(intent);
-		
-	}
 
+	public class AppsAdapter extends BaseAdapter {
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ImageView i = new ImageView(getApplicationContext());
+
+			final int pos = position;
+			
+			int info = mApps.get(position % mApps.size());
+
+			i.setImageResource(info);
+			i.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+			final int h = (int) (90 * getResources().getDisplayMetrics().density + 0.5f);
+			final int w = (int) (90 * getResources().getDisplayMetrics().density + 0.5f);
+			i.setLayoutParams(new GridView.LayoutParams(h, w));
+			
+			i.setOnClickListener(new OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					Toast.makeText(getApplicationContext(), pos + "", 3000).show();
+					Intent intent = new Intent(getApplicationContext(), BatonCreate_PostALastTask.class);
+					startActivity(intent);
+				}
+			});
+
+			return i;
+		}
+
+		public final int getCount() {
+			return Math.min(32, mApps.size());
+		}
+
+		public final Object getItem(int position) {
+			return mApps.get(position % mApps.size());
+		}
+
+		public final long getItemId(int position) {
+			return position;
+		}
+	}
 }
